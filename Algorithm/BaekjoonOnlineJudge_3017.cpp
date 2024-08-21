b0d5b9d1be68d501151f9d151d1714b9be6764c2
Taegon Lee <ltg9amer@naver.com>
2024-08-21 23:56:40 +0900

    20240821

--- Algorithm/BaekjoonOnlineJudge_3017.cpp
+++ Algorithm/BaekjoonOnlineJudge_3017.cpp
@@ -1,14 +1,90 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+vector<bool> isVisited;
+string a, b, closestNumber;
+bool solved;
+int bSize;
+
+void back_tracking_equal_upper(int currentIndex) {
+	if (closestNumber < a) {
+		return;
+	}
+
+	if (currentIndex >= bSize) {
+		solved = true;
+
+		return;
+	}
+
+	char currentNumber = ' ';
+
+	for (int i = 0; i < bSize; ++i) {
+		if (currentNumber != b[i] && !isVisited[i]) {
+			currentNumber = closestNumber[currentIndex] = b[i];
+			isVisited[i] = true;
+
+			back_tracking_equal_upper(currentIndex + 1);
+
+			if (solved) {
+				break;
+			}
+
+			isVisited[i] = false;
+		}
+	}
+}
+
+void back_tracking_lower(int currentIndex) {
+	if (closestNumber >= a) {
+		return;
+	}
+
+	if (currentIndex >= bSize) {
+		solved = true;
+
+		return;
+	}
+
+	char currentNumber = ' ';
+
+	for (int i = 0; i < bSize; ++i) {
+		if (!(!currentIndex && b[i] == '0') && currentNumber != b[i] && !isVisited[i]) {
+			currentNumber = closestNumber[currentIndex] = b[i];
+			isVisited[i] = true;
+
+			back_tracking_lower(currentIndex + 1);
+
+			if (solved) {
+				break;
+			}
+
+			isVisited[i] = false;
+		}
+	}
+}
+
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-
-	string a, b;
-
 	cin >> a >> b;
+
+	bSize = b.size();
+
+	sort(b.begin(), b.end());
+	isVisited.resize(bSize);
+	closestNumber.resize(bSize, '9');
+	back_tracking_equal_upper(0);
+
+	cout << (solved ? closestNumber : "0") << '\n';
+
+	solved = false;
+
+	sort(b.begin(), b.end(), greater<>());
+	fill(isVisited.begin(), isVisited.end(), false);
+	fill(closestNumber.begin(), closestNumber.end(), '0');
+	back_tracking_lower(0);
+
+	cout << (solved ? closestNumber : "0");
 }
